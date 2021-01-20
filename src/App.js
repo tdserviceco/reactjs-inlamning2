6015c7e4e6e9b530a4f4a291050c6216bf63e711
Tommy Danielsson <tdserviceco@gmail.com>
2021-01-20 21:53:57 +0100

    uppgift 2 klart.. små css adderat

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     endpoint = process.env.APIENDPOINT,
     [sdgData, updateDataFromSdgData] = useState([]),
     GetGoal = async () => {
-      return await Axios.get('https://unstats.un.org/SDGAPI/v1/sdg/Goal/List')
+      return await Axios.get(process.env.APIENDPOINT,)
     },
     SdgData = () => {
       return sdgData.map((data, key) => <DisplayDescriptions title={data.title} key={key} displayDescriptions={data.description} />);
